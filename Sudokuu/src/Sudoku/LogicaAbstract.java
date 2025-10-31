fd0d9d8c6608076eea8245e613c04e8b263523ea
Jiny-perez <jinyperez55@gmail.com>
2025-10-31 10:28:09 -0600

    AbstractLogica y Sudoku Logica lsita

--- Sudokuu/src/Sudoku/LogicaAbstract.java
+++ Sudokuu/src/Sudoku/LogicaAbstract.java
@@ -5,19 +5,12 @@ package Sudoku;
  * @author ljmc2
  */
 public abstract class LogicaAbstract {
+
     protected int[][] tablero = new int[9][9];
     protected int[][] solucion = new int[9][9];
-    protected boolean[][] fijos = new boolean[9][9];
-    protected int errores = 0;
+    protected boolean[][] predeterminado = new boolean[9][9];
+    protected int errores=0;
     
-    public int obtener(int f, int c) {
-        return tablero[f][c];
-    }
-
-    public boolean esFijo(int f, int c) {
-        return fijos[f][c];
-    }
-
     public int getErrores() {
         return errores;
     }
@@ -25,10 +18,26 @@ public abstract class LogicaAbstract {
     public void resetErrores() {
         errores = 0;
     }
-    
-    protected abstract void llenarTableroCompleto();
-    public abstract boolean esValido(int f, int c, int v);
+
+    public int[][] getSolucion() {
+        return solucion;
+    }
+   
+    public abstract int obtener(int row, int col);
+
+    public abstract boolean isPredeterminado(int row, int col);
+
+    public abstract boolean asignar(int row, int col, int val);
+
+    public abstract boolean remover(int row, int col);
+
+    public abstract boolean esValido(int row, int col, int val);
+
     public abstract void generar(int pistas);
+
     public abstract void limpiarEntradas();
+
     public abstract boolean isComplete();
+
+    protected abstract void llenarTableroCompleto();
 }

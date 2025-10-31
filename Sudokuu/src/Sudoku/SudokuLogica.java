abad6773d6cd39c1cbd8caa41963f7560300bd6f
Jiny-perez <jinyperez55@gmail.com>
2025-10-31 09:28:20 -0600

    Metodo llenar tablero

--- Sudokuu/src/Sudoku/SudokuLogica.java
+++ Sudokuu/src/Sudoku/SudokuLogica.java
@@ -1,13 +1,103 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Sudoku;
+
+import java.util.Random;
 
 /**
  *
  * @author marye
  */
-public class SudokuLogica {
-    
+public class SudokuLogica extends LogicaAbstract {
+
+    private int[][] tablero = new int[9][9];
+    private int[][] Solucion = new int[9][9];
+    private boolean[][] predeterminado = new boolean[9][9];
+    private int errores;
+
+    public int get(int row, int col) {
+        return tablero[row][col];
+    }
+
+    public boolean isPredeterminado(int row, int col) {
+        return predeterminado[row][col];
+    }
+
+    public int getErrores() {
+        return errores;
+    }
+
+    public void resetErrores() {
+        errores = 0;
+    }
+
+    public void set(int row, int col, int val) {
+       
+    }
+
+    public void Clear() {
+
+    }
+
+    public void cargar(int[][] puzzle, int[][] sol) {
+
+    }
+
+    public boolean esValido(int row, int col, int val) {
+
+    }
+
+    public boolean resuelto() {
+
+    }
+
+    public void revelarSolucion() {
+
+    }
+
+    public boolean rellenar() {
+        Random rand = new Random();
+        int row = 0, col = 0;
+
+        while (row < 9) {
+            if (Solucion[row][col] == 0) {
+                boolean colocado = false;
+                int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+                for (int i = 0; i < numeros.length; i++) {
+                    int j = rand.nextInt(numeros.length);
+                    int temp = numeros[i];
+                    numeros[i] = numeros[j];
+                    numeros[j] = temp;
+                }
+
+                for (int i = 0; i < numeros.length; i++) {
+                    int v = numeros[i];
+                    if (esValido(row, col, v)) {
+                        Solucion[row][col] = v;
+                        colocado = true;
+                        break;
+                    }
+                }
+
+                if (!colocado) {
+                    Solucion[row][col] = 0;
+                    if (col == 0) {
+                        row--;
+                        col = 8;
+                    } else {
+                        col--;
+                    }
+                    continue;
+                }
+            }
+
+            if (col == 8) {
+                row++;
+                col = 0;
+            } else {
+                col++;
+            }
+        }
+        return true;
+    }
+
 }
